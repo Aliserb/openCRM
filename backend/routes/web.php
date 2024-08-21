039a6f684b5a7b2bec09d29641f2f087e7934db9
veroni1337 <aliserb773@gmail.com>
2024-08-21 21:29:29 +0200

    Добавил тестовые роуты

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return 'Роут регистрации';
+});
+
+Route::get('/auth', function () {
+    return 'Роут авторизации';
+});
